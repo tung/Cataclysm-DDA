a8343b12b446f0721eb98e2c4b763b628a939b73
Tung Nguyen <tungtn3@gmail.com>
2020-06-13 13:48:38 +1000

    Show low-light bright colors in light gray in ASCII/Curses
    This at least makes it possible to see the yellow road lines on roads in
    moon light, instead of roads being totally invisible against surrounding
    grass.

--- src/map.cpp
+++ src/map.cpp
@@ -5827,7 +5827,14 @@ bool map::draw_maptile( const catacurses::window &w, const player &u, const trip
     } else if( u_vision[NV_GOGGLES] ) {
         tercol = ( bright_light ) ? c_white : c_light_green;
     } else if( low_light ) {
-        tercol = c_dark_gray;
+        if( tercol == c_white || tercol == c_light_gray ||
+            tercol == c_light_red || tercol == c_light_green ||
+            tercol == c_light_blue || tercol == c_light_cyan ||
+            tercol == c_pink || tercol == c_yellow ) {
+            tercol = c_light_gray;
+        } else {
+            tercol = c_dark_gray;
+        }
     } else if( u_vision[DARKNESS] ) {
         tercol = c_dark_gray;
     }
