29103e731714e2cf0f754a72585706845165fa0a
Tung Nguyen <tungtn3@gmail.com>
2020-07-28 18:13:46 +1000

    Always recheck memory of seen spaces in ASCII/Curses
    This extends the wall memory fix for ASCII/Curses from just walls to all
    visible spaces.  This fixes seeing a space filled with smoke or gas for
    the first time, having the smoke/gas dissipate, but remaining in map
    memory even when it later cleared.

--- src/map.cpp
+++ src/map.cpp
@@ -5665,13 +5665,10 @@ bool map::draw_maptile( const catacurses::window &w, const player &u, const trip
     bool hi = false;
     bool graf = false;
     bool draw_item_sym = false;
-    bool recheck_memory = false;
 
     int terrain_sym;
     if( curr_ter.has_flag( TFLAG_AUTO_WALL_SYMBOL ) ) {
         terrain_sym = determine_wall_corner( p );
-        // Wall sym memory may change based on seeing new surrounding walls.
-        recheck_memory = true;
     } else {
         terrain_sym = curr_ter.symbol();
     }
@@ -5806,13 +5803,7 @@ bool map::draw_maptile( const catacurses::window &w, const player &u, const trip
         }
     }
 
-    bool force_refresh_memory = false;
-    if( recheck_memory ) {
-        int memorized_sym = g->u.get_memorized_symbol( getabs( p ) );
-        force_refresh_memory = memorized_sym != memory_sym;
-    }
-
-    if( check_and_set_seen_cache( p ) || force_refresh_memory ) {
+    if( check_and_set_seen_cache( p ) || g->u.get_memorized_symbol( getabs( p ) ) != memory_sym ) {
         g->u.memorize_symbol( getabs( p ), memory_sym );
     }
 
