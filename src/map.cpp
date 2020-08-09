f3c0825b17619dcc8a9457584e08a9807544bbe7
Tung Nguyen <tungtn3@gmail.com>
2020-08-09 16:52:50 +1000

    Revert "Draw curses map memory in dark gray and not brown"
    This reverts commit 5d20104f80691159ac61f02f5a02982258c3599b.

--- src/map.cpp
+++ src/map.cpp
@@ -5520,9 +5520,9 @@ bool map::draw_maptile_from_memory( const catacurses::window &w, const tripoint 
         const int k = p.x + getmaxx( w ) / 2 - view_center.x;
         const int j = p.y + getmaxy( w ) / 2 - view_center.y;
 
-        mvwputch( w, point( k, j ), c_dark_gray, sym );
+        mvwputch( w, point( k, j ), c_brown, sym );
     } else {
-        wputch( w, c_dark_gray, sym );
+        wputch( w, c_brown, sym );
     }
     return true;
 }
